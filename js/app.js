ecfaa0cb72538bb66fc0ead5241837310f53e92f
Matt Oakes <mattoakes@ACA80183.ipt.aol.com>
2016-10-29 15:23:31 -0700

    First Commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+//Problem is user interaction doesnt provide desired results//
+// Add user activaity so user can do basic tasks//
+
+// Add a new task  
+var addTask = function() {
+  //When the button is pressed 
+  //Create a new list itemwith the text from new task
+    //Input checkbox
+    //Label
+    //Input(text)
+    //button.edit
+    //button.delete
+    // Each elements, needs modified and appended
+}    
+
+// Edit an existing task
+var editTask = function() {
+  //When the edit button is pressed 
+    //if the class of the parent is edit mode
+      //Switch back from edit mode
+      //Make the label text become the inputs value
+    //else
+      //Switch to editmode
+      //input value becomes labels text
+
+    //Toggle editMode on the parent  
+}      
+
+//Delete an existing task
+var deleteTask = function() {
+  ////When the delete button is pressed
+    //Remove parent list item from the ol 
+}
+//Mark a task as complaet
+var taskCompleted = function() {
+  //When the check box is checked
+    //Append the task list item to the complete tasks
+}
+
+
+//Mark a task as incomplete
+var taskinComplete = function() {
+  //When the check box is unchecked
+    //Append the task list item to the incomplete tasks
+}
+
+
+
+
